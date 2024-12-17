bd688180aeaa6562dc76dac3b43b6786974adec3
coderjc <2740954762@qq.com>
2024-12-18 00:57:33 +0800

    feat: add case

---
+++ 3. monorepo/basic/monorepo/apps/project-1/src/index.js
@@ -0,0 +1,3 @@
+import { add } from '@monorepo/utils'
+
+console.log('project-1:', add(12, 24))
---
+++ 3. monorepo/basic/monorepo/apps/project-2/src/index.js
@@ -0,0 +1,3 @@
+import { subtract } from '@monorepo/utils'
+
+console.log('project-2:', subtract(10, 20))
