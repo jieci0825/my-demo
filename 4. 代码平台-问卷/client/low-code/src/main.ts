f64955478a63912a1347a3e9f95aa7ab626f6f9a
coderjc <2740954762@qq.com>
2024-12-26 15:18:58 +0800

    feat: 完善项目结构

--- 4. 代码平台-问卷/client/low-code/src/main.ts
+++ 4. 代码平台-问卷/client/low-code/src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import '@/styles/index.scss'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(createPinia())
+
+app.mount('#app')
--- 4. 代码平台-问卷/server/low-code/src/core/init.js
+++ 4. 代码平台-问卷/server/low-code/src/core/init.js
@@ -7,51 +7,43 @@ const { sequelize } = require('./db')
 const router = new Router({ prefix: '/api' })
 
 class InitModule {
-	static initMain(app) {
-		InitModule.app = app
-		InitModule.rootPath = process.cwd()
+    static initMain(app) {
+        InitModule.app = app
+        InitModule.rootPath = process.cwd()
 
-		InitModule.initLoadRouters()
-		InitModule.initConfig()
-		InitModule.initTableModels()
-	}
+        InitModule.initLoadRouters()
+        InitModule.initConfig()
+        InitModule.initTableModels()
+    }
 
-	// 挂载全局配置
-	static initConfig() {
-		const configPath = path.resolve(InitModule.rootPath, 'src/config/global.config')
-		global.config = require(configPath)
-	}
+    // 挂载全局配置
+    static initConfig() {
+        const configPath = path.resolve(InitModule.rootPath, 'src/config/global.config')
+        global.config = require(configPath)
+    }
 
-	// 加载全部路由
-	static initLoadRouters() {
-		function loadRouteModule(obj) {
-			if (obj instanceof Router) {
-				router.use(obj.routes())
-			}
-		}
+    // 加载全部路由
+    static initLoadRouters() {
+        function loadRouteModule(obj) {
+            if (obj instanceof Router) {
+                router.use(obj.routes())
+            }
+        }
 
-		InitModule.app.use(router.routes()).use(router.allowedMethods())
+        InitModule.app.use(router.routes()).use(router.allowedMethods())
 
-		const routerPath = path.resolve(InitModule.rootPath, 'src/app/apis')
-		requireDirectory(module, routerPath, {
-			visit: loadRouteModule
-		})
-	}
+        const routerPath = path.resolve(InitModule.rootPath, 'src/app/apis')
+        requireDirectory(module, routerPath, {
+            visit: loadRouteModule
+        })
+    }
 
-	// 初始化数据表模型
-	static async initTableModels() {
-		const modelPath = path.resolve(InitModule.rootPath, 'src/app/models')
-		requireDirectory(module, modelPath)
-		await sequelize.sync()
-		await InitModule.initData()
-	}
-
-	// 初始化数据
-	static async initData() {
-		const data = { username: 'zhangsan', password: '123456' }
-		if (await Demo.findOne({ where: { username: data.username } })) return
-		await Demo.create(data)
-	}
+    // 初始化数据表模型
+    static async initTableModels() {
+        const modelPath = path.resolve(InitModule.rootPath, 'src/app/models')
+        requireDirectory(module, modelPath)
+        await sequelize.sync()
+    }
 }
 
 module.exports = InitModule
