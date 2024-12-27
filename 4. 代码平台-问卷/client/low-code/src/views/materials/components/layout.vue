fe45edb06cf71bd62061a42b2d6c4e6770acda3c
coderjc <2740954762@qq.com>
2024-12-27 12:09:46 +0800

    feat: 完成三栏布局组件

---
+++ 4. 代码平台-问卷/client/low-code/src/views/materials/components/layout.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="layout-container flex">
+        <!-- 选择业务组件 -->
+        <div class="left p-20">
+            <slot></slot>
+        </div>
+        <!-- 展示业务组件 -->
+        <div class="center p-30">
+            <slot name="center"></slot>
+        </div>
+        <!-- 编辑业务组件 -->
+        <div class="right p-20">
+            <slot name="right"></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.layout-container {
+    height: 100%;
+    height: 100%;
+    overflow: hidden;
+    align-items: flex-start;
+    border: 1px solid var(--border-color);
+    border-top-right-radius: var(--border-radius-large);
+    border-bottom-right-radius: var(--border-radius-large);
+    border-bottom-left-radius: var(--border-radius-large);
+    .left {
+        width: 180px;
+        height: 100%;
+    }
+    .center {
+        flex: 1;
+        height: 100%;
+        overflow-y: auto;
+        border-left: 1px solid var(--border-color);
+    }
+    .right {
+        width: 350px;
+        height: 100%;
+        border-left: 1px solid var(--border-color);
+    }
+}
+</style>
