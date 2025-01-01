24f7bf229e44c5d186c527c2c7e7c941e0377d2b
coderjc <2740954762@qq.com>
2025-01-01 15:04:04 +0800

    feat: 增加文本节点路由及优化路由结构

--- 4. 代码平台-问卷/client/low-code/src/router/index.ts
+++ 4. 代码平台-问卷/client/low-code/src/router/index.ts
@@ -1,96 +1,8 @@
-import { JC_WEN_JUAN_ACTIVE_VIEW, SINGLE_PIC_SELECT_KEY, SINGLE_SELECT_KEY } from '@/constants'
+import { JC_WEN_JUAN_ACTIVE_VIEW, SINGLE_PIC_SELECT_KEY, SINGLE_SELECT_KEY, TEXT_NODE_KEY } from '@/constants'
 import { useMaterialStore } from '@/stores/use-material'
-import type { MaterialKeys } from '@/types/materials'
 import { createWebHistory, createRouter } from 'vue-router'
-
-const routes = [
-    {
-        path: '/',
-        meta: {
-            title: '首页'
-        },
-        component: () => import('@/views/home/index.vue')
-    },
-    {
-        path: '/editor/:mode',
-        name: 'editor',
-        meta: {
-            title: '问卷'
-        },
-        component: () => import('@/views/editor/index.vue')
-    },
-    {
-        path: '/materials',
-        name: 'materials',
-        meta: {
-            title: '组件市场'
-        },
-        redirect: '/materials/select',
-        component: () => import('@/views/materials/index.vue'),
-        children: [
-            {
-                path: 'input',
-                name: 'materials-input',
-                component: () => import('@/views/materials/components/input-group.vue')
-            },
-            {
-                path: 'advanced',
-                name: 'materials-advanced',
-                component: () => import('@/views/materials/components/advanced-group.vue')
-            },
-            {
-                path: 'contact',
-                name: 'materials-contact',
-                component: () => import('@/views/materials/components/contact-group.vue')
-            },
-            {
-                path: 'note',
-                name: 'materials-note',
-                component: () => import('@/views/materials/components/note-group.vue')
-            },
-            {
-                path: 'personal-info',
-                name: 'materials-personal-info',
-                component: () => import('@/views/materials/components/personal-info-group.vue')
-            },
-            {
-                path: 'select',
-                name: 'materials-select',
-                component: () => import('@/views/materials/components/select-group.vue'),
-                redirect: '/materials/select/single',
-                children: [
-                    {
-                        path: 'single',
-                        name: 'materials-select-single',
-                        component: () => import('@/components/survey-comps/materials/select-comps/single-select.vue')
-                    },
-                    {
-                        path: 'multiple',
-                        name: 'materials-select-multiple',
-                        component: () => import('@/components/survey-comps/materials/select-comps/multiple-select.vue')
-                    },
-                    {
-                        path: 'option',
-                        name: 'materials-select-option',
-                        component: () => import('@/components/survey-comps/materials/select-comps/option-select.vue')
-                    },
-                    {
-                        path: 'single-pic',
-                        name: 'materials-select-single-pic',
-                        component: () =>
-                            import('@/components/survey-comps/materials/select-comps/single-pic-select.vue')
-                    },
-                    {
-                        path: 'multiple-pic',
-                        name: 'materials-select-multiple-pic',
-                        component: () =>
-                            import('@/components/survey-comps/materials/select-comps/multiple-pic-select.vue')
-                    }
-                ]
-            }
-        ]
-    }
-]
+import { routes } from './routes'
+import type { MaterialKeys } from '@/types/materials'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -99,7 +11,8 @@ const router = createRouter({
 
 const routeNameToMaterialCompKeyMap: { [key: string]: MaterialKeys } = {
     'materials-select-single': SINGLE_SELECT_KEY,
-    'materials-select-single-pic': SINGLE_PIC_SELECT_KEY
+    'materials-select-single-pic': SINGLE_PIC_SELECT_KEY,
+    'materials-node-text': TEXT_NODE_KEY
 }
 
 router.beforeEach((to, _, next) => {
---
+++ 4. 代码平台-问卷/client/low-code/src/router/routes.ts
@@ -0,0 +1,95 @@
+export const routes = [
+    {
+        path: '/',
+        meta: {
+            title: '首页'
+        },
+        component: () => import('@/views/home/index.vue')
+    },
+    {
+        path: '/editor/:mode',
+        name: 'editor',
+        meta: {
+            title: '问卷'
+        },
+        component: () => import('@/views/editor/index.vue')
+    },
+    {
+        path: '/materials',
+        name: 'materials',
+        meta: {
+            title: '组件市场'
+        },
+        redirect: '/materials/select',
+        component: () => import('@/views/materials/index.vue'),
+        children: [
+            {
+                path: 'input',
+                name: 'materials-input',
+                component: () => import('@/views/materials/components/input-group.vue')
+            },
+            {
+                path: 'advanced',
+                name: 'materials-advanced',
+                component: () => import('@/views/materials/components/advanced-group.vue')
+            },
+            {
+                path: 'contact',
+                name: 'materials-contact',
+                component: () => import('@/views/materials/components/contact-group.vue')
+            },
+            {
+                path: 'note',
+                name: 'materials-note',
+                component: () => import('@/views/materials/components/note-group.vue'),
+                children: [
+                    {
+                        path: 'text',
+                        name: 'materials-node-text',
+                        component: () => import('@/components/survey-comps/materials/node-comps/text-node.vue')
+                    }
+                ]
+            },
+            {
+                path: 'personal-info',
+                name: 'materials-personal-info',
+                component: () => import('@/views/materials/components/personal-info-group.vue')
+            },
+            {
+                path: 'select',
+                name: 'materials-select',
+                component: () => import('@/views/materials/components/select-group.vue'),
+                redirect: '/materials/select/single',
+                children: [
+                    {
+                        path: 'single',
+                        name: 'materials-select-single',
+                        component: () => import('@/components/survey-comps/materials/select-comps/single-select.vue')
+                    },
+                    {
+                        path: 'multiple',
+                        name: 'materials-select-multiple',
+                        component: () => import('@/components/survey-comps/materials/select-comps/multiple-select.vue')
+                    },
+                    {
+                        path: 'option',
+                        name: 'materials-select-option',
+                        component: () => import('@/components/survey-comps/materials/select-comps/option-select.vue')
+                    },
+                    {
+                        path: 'single-pic',
+                        name: 'materials-select-single-pic',
+                        component: () =>
+                            import('@/components/survey-comps/materials/select-comps/single-pic-select.vue')
+                    },
+                    {
+                        path: 'multiple-pic',
+                        name: 'materials-select-multiple-pic',
+                        component: () =>
+                            import('@/components/survey-comps/materials/select-comps/multiple-pic-select.vue')
+                    }
+                ]
+            }
+        ]
+    }
+]
