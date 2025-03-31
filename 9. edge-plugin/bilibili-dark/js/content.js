76ec5e6ba8148bb4bc2de065538473fbb74fd04c
coderjc <2740954762@qq.com>
2025-04-01 06:02:52 +0800

    refactor: 切换为css控制主题

--- 9. edge-plugin/bilibili-dark/js/content.js
+++ 9. edge-plugin/bilibili-dark/js/content.js
@@ -51,27 +51,7 @@ const JC_BILIBILI_IS_DARK_KEY = 'JC_BILIBILI_IS_DARK'
     })
 
     const container = document.documentElement
-    const a1s = document.querySelectorAll('body a')
-    const styleId = '__jc_dark_centent_style__'
 
-    const cssDarkMap = {
-        '--Wh0': '#23272f',
-        '--Ga11': '#23272f',
-        '--Ga1': '#333a45',
-        '--Ga7': '#ebecf0',
-        '--Ga1_s': '#343a46',
-        '--Ga10': '#f6f7f9',
-        '--Ga0_s': '#343a46',
-        '--Ga2': '#343a46',
-        '--Ga0': '#343a46',
-        '--Ga12': '#343a46',
-        '--Ga12_s': '#343a46',
-        '--Ga13_s': '#343a46',
-        '--brand_blue_thin': '#333A45'
-    }
-
-    // tag-color #9499A0
-    // tag-bg #343A46
     function init() {
         const isDark = getLocal(JC_BILIBILI_IS_DARK_KEY)
         // 为 null 表示是第一次打开，因此默认设置为暗黑模式
@@ -84,34 +64,13 @@ const JC_BILIBILI_IS_DARK_KEY = 'JC_BILIBILI_IS_DARK'
 
     function openDark() {
         setLocal(JC_BILIBILI_IS_DARK_KEY, true)
-        coverCssVariable()
-        // 还有一些需要单独设置
-        for (const a of a1s) {
-            a.style.setProperty('color', '#ebecf0')
-        }
-
-        // 给当前页面添加一个 style
-        const style = document.createElement('style')
-        style.id = styleId
-        style.innerHTML = `.float-button.float-button{
-        box-shadow: 0 0 10px  #727b7e;
-    }`
-        container.appendChild(style)
-    }
-
-    function coverCssVariable() {
-        for (let key in cssDarkMap) {
-            container.style.setProperty(key, cssDarkMap[key])
-        }
+        // 给 html 添加自定义属性 data-theme，值为 dark
+        container.setAttribute('data-theme', 'dark')
     }
 
     function closeDark() {
         setLocal(JC_BILIBILI_IS_DARK_KEY, false)
-        for (let key in cssDarkMap) {
-            container.style.removeProperty(key)
-        }
-        for (const a of a1s) {
-            a.style.removeProperty('color')
-        }
+        // 移除 html 的自定义属性 data-theme
+        container.removeAttribute('data-theme')
     }
 })()
