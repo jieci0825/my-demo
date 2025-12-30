9f1107252aa448b67a84004d67838b44c9a6590c
coderjc <coderjc@qq.com>
2025-12-30 14:57:06 +0800

    feat: 在主入口文件中添加开发环境下的数据库文档日志输出
    - 在 bootstrap 函数中引入 dbTool 和 isDev，条件性地输出数据库中的所有文档，便于开发调试

--- 025_utools-plugin/book-manager/src/main.js
+++ 025_utools-plugin/book-manager/src/main.js
@@ -1,10 +1,19 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+
+import { createApp } from 'vue'
+import { dbTool } from './utils'
+import { isDev } from './configs'
+
 import './styles/index.scss'
 
 function bootstrap() {
     const app = createApp(App)
     app.mount('#app')
+
+    if (isDev) {
+        const docs = dbTool.allDocs()
+        console.log('DB: ', docs)
+    }
 }
 
 bootstrap()
