a644b3e194a59e20fe85e3124f6178d0d9c842e6
coderjc <coderjc@qq.com>
2026-01-02 15:06:55 +0800

    feat: 初始化 Rollup 项目结构
    - 添加 .gitignore 文件以排除不必要的文件
    - 创建 package.json 文件，定义项目基本信息和开发依赖
    - 添加 Rollup 配置文件，设置入口和出口文件
    - 实现基本的数学运算模块和示例入口文件

---
+++ 026_rollup/01_quick-start/rollup.config.js
@@ -0,0 +1,11 @@
+export default {
+    // 入口文件
+    input: 'src/index.js',
+    // 出口文件
+    output: {
+        // 出口文件路径
+        file: 'dist/bundle.js',
+        // 出口文件格式
+        format: 'es'
+    }
+}
