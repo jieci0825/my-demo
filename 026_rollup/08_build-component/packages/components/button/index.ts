71d81b88a42f3df27452711f0c9ecf636990d684
coderjc <coderjc@qq.com>
2026-01-13 12:11:55 +0800

    chore: 移除demo

--- 026_rollup/08_build-component/packages/components/button/index.ts
+++
@@ -1,8 +0,0 @@
-import { withInstall } from '@my/utils'
-import _MyButton from './src/button.vue'
-
-export const MyButton = withInstall(_MyButton, 'MyButton')
-
-export default MyButton
-
-export * from './src/types'
--- 026_rollup/08_build-component/play/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import MyComp from '@my/my-comp'
-import '@my/theme-chalk/index.css'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).use(MyComp).mount('#app')
