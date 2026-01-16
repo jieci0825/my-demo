0f930efc3f6ea3c281bf1db296cae422da70c559
coderjc <coderjc@qq.com>
2026-01-16 12:32:30 +0800

    feat: 新增 LangChain 示例项目，展示高亮关键词功能和可运行链条
    - 创建两个示例文件，分别展示如何使用 ChatOllama 模型和高亮关键词函数
    - 引入 RunnableSequence 以支持链式调用，增强示例的灵活性和可读性
    - 通过高亮关键词函数提升终端输出的可视化效果

---
+++ 028_LangChain/07_Runnable/关键词高亮-数组.js
@@ -0,0 +1,35 @@
+import { StringOutputParser } from '@langchain/core/output_parsers'
+import { PromptTemplate } from '@langchain/core/prompts'
+import { RunnableLambda, RunnableSequence } from '@langchain/core/runnables'
+import { ChatOllama } from '@langchain/ollama'
+
+// 高亮关键词函数
+const highlightKeywords = input => {
+    // 因为是在终端查看，所以用*&&*包裹关键词表示高亮
+    return input.replace(/(闭包)/g, '*&&*$1*&&*')
+}
+
+const highlightKeywordsRunnable = RunnableLambda.from(highlightKeywords)
+
+const model = new ChatOllama({
+    model: 'qwen2.5:7b',
+    baseURL: 'http://localhost:11434'
+})
+
+// 创建提示词模板
+const prompt = PromptTemplate.fromTemplate('请用中文解释一下什么是:{question}')
+
+// 解析器
+const parser = new StringOutputParser()
+
+// 创建链
+let chain = RunnableSequence.from([
+    prompt,
+    model,
+    parser,
+    highlightKeywordsRunnable
+])
+
+// 无法使用流式输出
+const res = await chain.invoke({ question: '什么是闭包' })
+console.log(res)
