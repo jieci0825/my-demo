ab225ebb0670418b2e3f5d8ed624cdf531ec29e4
coderjc <coderjc@qq.com>
2026-01-17 12:44:56 +0800

    feat: 搭建基础框架

---
+++ 028_LangChain/10_RAG/frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container">
+        <h1>Hello World</h1>
+        <h2>大河之剑天上来</h2>
+        <el-button type="primary">Click me</el-button>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
