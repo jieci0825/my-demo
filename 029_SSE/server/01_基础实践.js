b1bd6d38057a50288489f7b08e1c880a9b8c6082
coderjc <coderjc@qq.com>
2026-01-28 17:50:55 +0800

    feat: 增加自定义事件处理和断开连接

--- 029_SSE/server/01_基础实践.js
+++ 029_SSE/server/01_基础实践.js
@@ -31,7 +31,7 @@ router.get('/api/data', async ctx => {
     ctx.res.write(`id: ${messageId}\n`)
 
     // 每隔1秒发送一次数据
-    setInterval(() => {
+    let timer = setInterval(() => {
         const content = {
             time: new Date().toISOString(),
             message: 'Hello, world!',
@@ -39,11 +39,34 @@ router.get('/api/data', async ctx => {
         ctx.res.write(`data: ${JSON.stringify(content)}\n\n`)
     }, 1000)
 
+    const sendEvent = (event, data) => {
+        ctx.res.write(`event: ${event}\n`)
+        ctx.res.write(`data: ${JSON.stringify(data)}\n\n`)
+    }
+
     setTimeout(() => {
-        // 10秒后关闭连接
+        sendEvent('notification', {
+            time: new Date().toISOString(),
+            message: '大河之剑天上来',
+        })
+    }, 2000)
+
+    setTimeout(() => {
+        sendEvent('done', {
+            time: new Date().toISOString(),
+            message: '连接关闭',
+        })
         ctx.res.end()
         console.log('Connection closed')
-    }, 10000)
+    }, 5000)
+
+    // 监听浏览器断开连接
+    ctx.req.on('close', () => {
+        // 浏览器断开连接了
+        clearInterval(timer)
+        console.log('浏览器断开连接')
+        ctx.res.end()
+    })
 })
 
 app.use(router.routes())
